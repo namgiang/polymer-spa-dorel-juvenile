d10c3bb3a0b7adbc23c3b34bf3fbc011f448a316
Marnix Harderwijk <m.harderwijk@littlebigagency.com>
2017-03-30 15:59:11 +0200

    DIO-409: add app-config and sample file for endpoint usage cms, remove debug variable in app, include app-config.js in index

---
+++ app-config.js
@@ -0,0 +1,8 @@
+/**
+ * @constant
+ * @name CONFIG
+ * @description all constants for the application to use 
+ */
+var CONFIG = {
+  CMS_URL: "//polymer-wp.dev/wp-json" // build script will change this
+};
