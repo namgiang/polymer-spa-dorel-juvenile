26505368a3dc4ee9c7986b8e8a26909a421c2581
Peter Curet <petercuret@gmail.com>
2017-04-10 15:55:42 +0200

    DIO-619: #comment shared styles are now imported in the main app, from which they propagate to the rest of the components. The themes can be switched by changing app-config.js

--- app-config.js
+++ app-config.js
@@ -4,5 +4,6 @@
  * @description all constants for the application to use 
  */
 var CONFIG = {
-  CMS_URL: "//dorel-wp.dev/wp-json" // build script will change this
+  CMS_URL: "//dorel-wp.dev/wp-json",// build script will change this
+  BRAND_NAME: 'maxicosi', // Determines the shares brand themes
 };
