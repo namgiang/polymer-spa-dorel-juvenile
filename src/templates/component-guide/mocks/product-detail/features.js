bae51e0f4c13a40c9bbb8e88f639874fe178304b
Peter Curet <petercuret@gmail.com>
2017-10-03 11:33:24 +0200

    DIO-1739: Added features to the component guide

---
+++ src/templates/component-guide/mocks/product-detail/features.js
@@ -0,0 +1,35 @@
+var featuresMock = {
+  title: 'This is a Feature title',
+  features: [
+    {
+      image: 'https://d2csxpduxe849s.cloudfront.net/media/2A82AC0B-80EE-4791-AE41EE17E21FC96A/7A6E1176-C622-4D7B-98EA3FF1638F6E0A/webimage-87C4A5B5-8C2D-4943-A8627D7504EE278C.png',
+      title: 'Lorem ipsum dolor sit amet',
+      description: 'Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero.'
+    },
+    {
+      image: 'https://d2csxpduxe849s.cloudfront.net/media/2A82AC0B-80EE-4791-AE41EE17E21FC96A/CF2AE7EB-69AF-4888-B34B95EC26ADD040/webimage-A1A31EDD-2A75-43D4-AE9F432EB6D0C646.png',
+      title: 'Nullam quis ante',
+      description: 'In ac dui quis mi consectetuer lacinia'
+    },
+    {
+      image: 'https://d2csxpduxe849s.cloudfront.net/media/2A82AC0B-80EE-4791-AE41EE17E21FC96A/79D625C0-00C5-4EB1-9742A072F69BB3B5/webimage-1842122E-3B05-458D-8718CE35C62964A4.png',
+      title: 'Duis arcu tortor',
+      description: 'Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris'
+    },
+    {
+      image: 'https://d2csxpduxe849s.cloudfront.net/media/2A82AC0B-80EE-4791-AE41EE17E21FC96A/DE6B3FE5-FCA7-4FFF-924E8D3045075486/webimage-D5564CB7-B9A8-4275-A42DE523CA93983C.png',
+      title: 'Lorem ipsum dolor sit amet',
+      description: 'Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero.'
+    },
+    {
+      image: 'https://d2csxpduxe849s.cloudfront.net/media/2A82AC0B-80EE-4791-AE41EE17E21FC96A/C9FF0B86-7F64-464C-A2F91658A9B753D3/webimage-63C89C61-0DA4-4360-AD196F97D2AADA2D.png',
+      title: 'Nullam quis ante',
+      description: 'In ac dui quis mi consectetuer lacinia'
+    },
+    {
+      image: 'https://d2csxpduxe849s.cloudfront.net/media/2A82AC0B-80EE-4791-AE41EE17E21FC96A/67684616-CB1A-4493-BF10AB9DF9EDFAAB/webimage-A3861B24-1B28-42A8-99E59C09A899ED6E.png',
+      title: 'Duis arcu tortor',
+      description: 'Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris'
+    }
+  ]
+};
