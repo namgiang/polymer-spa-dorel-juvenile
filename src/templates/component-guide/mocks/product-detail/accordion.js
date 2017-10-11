1ee8daf4ddcb43bb85552880ca335052d71772bd
Peter Curet <petercuret@gmail.com>
2017-10-11 15:12:17 +0200

    DIO-1831: Added FAQ mock data and improved the accordions for responsive behaviour.

--- src/templates/component-guide/mocks/product-detail/accordion.js
+++ src/templates/component-guide/mocks/product-detail/accordion.js
@@ -3,14 +3,31 @@ var accordionMock = {
     {
       type: 'basic',
       heading: 'Basic Content',
-      description: "<p>The new Vela offers Maxi-Cosi&rsquo;s ISOFIX compatible system, ISOGO&trade;, a patented ISOFIX compatible soft latch system, which enables you to secure your child&rsquo;s car seat quick and safely in 3 simple steps and gives you confidence your seat is always correctly installed.</p>\r\n<p>The Hera also comes with the Maxi-Cosi unique &lsquo;Cool Baby&rsquo; wicking fabric, to keep your baby cool and comfortable. Rearward facing up to approximately 12 months and forward facing to approximately 4 years, this is a great long lasting car seat. Still featuring Air Protect&trade; Superior Side Impact Technology to shield your child's head from side impact forces, it&rsquo;s the Hera you know and love, but better!</p>"},
+      description: "<p>The new Vela offers Maxi-Cosi&rsquo;s ISOFIX compatible system, ISOGO&trade;, a patented ISOFIX compatible soft latch system, which enables you to secure your child&rsquo;s car seat quick and safely in 3 simple steps and gives you confidence your seat is always correctly installed.</p>\r\n<p>The Hera also comes with the Maxi-Cosi unique &lsquo;Cool Baby&rsquo; wicking fabric, to keep your baby cool and comfortable. Rearward facing up to approximately 12 months and forward facing to approximately 4 years, this is a great long lasting car seat. Still featuring Air Protect&trade; Superior Side Impact Technology to shield your child's head from side impact forces, it&rsquo;s the Hera you know and love, but better!</p>"
+    },
     {
       type: 'basic',
       heading: 'Basic Content with Image',
       image_src: 'https://d2csxpduxe849s.cloudfront.net/media/2A82AC0B-80EE-4791-AE41EE17E21FC96A/862871BF-D6D4-4C8E-83E267C19C12528A/webimage-2348576A-48DE-45F9-9AB1E3B5664055C7.png',
       extra_link_html: '<p><a href="">Read more</a></p>',
-      description: "<p>The new Vela offers Maxi-Cosi&rsquo;s ISOFIX compatible system, ISOGO&trade;, a patented ISOFIX compatible soft latch system, which enables you to secure your child&rsquo;s car seat quick and safely in 3 simple steps and gives you confidence your seat is always correctly installed.</p>\r\n<p>The Hera also comes with the Maxi-Cosi unique &lsquo;Cool Baby&rsquo; wicking fabric, to keep your baby cool and comfortable. Rearward facing up to approximately 12 months and forward facing to approximately 4 years, this is a great long lasting car seat. Still featuring Air Protect&trade; Superior Side Impact Technology to shield your child's head from side impact forces, it&rsquo;s the Hera you know and love, but better!</p>"},
-    {type: 'faq', heading: 'FAQ Content', description: 'Lorem ipsum description'},
+      description: "<p>The new Vela offers Maxi-Cosi&rsquo;s ISOFIX compatible system, ISOGO&trade;, a patented ISOFIX compatible soft latch system, which enables you to secure your child&rsquo;s car seat quick and safely in 3 simple steps and gives you confidence your seat is always correctly installed.</p>\r\n<p>The Hera also comes with the Maxi-Cosi unique &lsquo;Cool Baby&rsquo; wicking fabric, to keep your baby cool and comfortable. Rearward facing up to approximately 12 months and forward facing to approximately 4 years, this is a great long lasting car seat. Still featuring Air Protect&trade; Superior Side Impact Technology to shield your child's head from side impact forces, it&rsquo;s the Hera you know and love, but better!</p>"
+    },
+    {
+      type: 'faq',
+      heading: 'FAQ Content',
+      extra_link_html: '<p>Your question not listed? <a href="">Contact support</a> or <a href="">Go to full FAQ</a></p>',
+      faq_items: [
+        {
+          title: 'Lorem Ipsum',
+          description: "<p>The new Vela offers Maxi-Cosi&rsquo;s ISOFIX compatible system, ISOGO&trade;, a patented ISOFIX compatible soft latch system, which enables you to secure your child&rsquo;s car seat quick and safely in 3 simple steps and gives you confidence your seat is always correctly installed.</p>\r\n<p>The Hera also comes with the Maxi-Cosi unique &lsquo;Cool Baby&rsquo; wicking fabric, to keep your baby cool and comfortable. Rearward facing up to approximately 12 months and forward facing to approximately 4 years, this is a great long lasting car seat. Still featuring Air Protect&trade; Superior Side Impact Technology to shield your child's head from side impact forces, it&rsquo;s the Hera you know and love, but better!</p>",
+          extra_link_html: '<p><a href="">Read more</a></p>'
+        },
+        {
+          title: 'This is another header',
+          description: "<p>The new Vela offers Maxi-Cosi&rsquo;s ISOFIX compatible system, ISOGO&trade;, a patented ISOFIX compatible soft latch system, which enables you to secure your child&rsquo;s car seat quick and safely in 3 simple steps and gives you confidence your seat is always correctly installed.</p>\r\n<p>The Hera also comes with the Maxi-Cosi unique &lsquo;Cool Baby&rsquo; wicking fabric, to keep your baby cool and comfortable. Rearward facing up to approximately 12 months and forward facing to approximately 4 years, this is a great long lasting car seat. Still featuring Air Protect&trade; Superior Side Impact Technology to shield your child's head from side impact forces, it&rsquo;s the Hera you know and love, but better!</p>",
+          extra_link_html: '<p><a href="">Read more</a></p>'
+        }]
+    },
     {
       type: 'manuals', heading: 'Manual Content', files: [
       {
