7aa98edcac3dd1044ee831dafad3a84bcebca825
Peter Curet <petercuret@gmail.com>
2017-10-19 14:59:40 +0200

    POC-P2.2: Fixed tabbed navigation in component guide. Removed paper-item dependency

--- src/templates/component-guide/pages/dorel-component-guide-basic-data.js
+++ src/templates/component-guide/pages/dorel-component-guide-basic-data.js
@@ -1,73 +1,38 @@
-var basicComponents = {
-  "id": 1,
-  "brand_id": 4,
-  "name": "Services",
-  "value": "services",
-  "sub_categories": [
-    {
-      "id": 10,
-      "service_topics_category_id": 1,
-      "name": "Introduction",
-      "items": [
-        {
-          "id": 1,
-          "service_topics_sub_category_id": 10,
-          "featured": 1,
-          "question": "Repairs1 - First Lorem ipsum this?",
-          "answer": "Lorem ipsum descrpition"
-        },
-
-        {
-          "id": 2,
-          "service_topics_sub_category_id": 10,
-          "featured": 0,
-          "question": "Repairs2 - Fourth Lorem ipsum this?",
-          "answer": "Lorem ipsum descrpition"
-        }
-      ]
-    },
-
-    {
-      "id": 20,
-      "service_topics_category_id": 1,
-      "name": "Typography",
-      "items": [
-        {
-          "id": 3,
-          "service_topics_sub_category_id": 20,
-          "featured": true,
-          "question": "SpareParts1 - Second Lorem ipsum this?",
-          "answer": "Lorem ipsum descrpition"
-        },
-        {
-          "id": 4,
-          "service_topics_sub_category_id": 20,
-          "question": "SpareParts2 - Second Lorem ipsum this?",
-          "answer": "Lorem ipsum descrpition"
-        }
-      ]
-    },
-
-    {
-      "id": 30,
-      "service_topics_category_id": 1,
-      "name": "Layout & Sizes",
-      "items": [
-        {
-          "id": 5,
-          "service_topics_sub_category_id": 30,
-          "featured": true,
-          "question": "HipSafetyService1 - Second Lorem ipsum this?",
-          "answer": "Lorem ipsum descrpition"
-        },
-        {
-          "id": 6,
-          "service_topics_sub_category_id": 30,
-          "featured": true,
-          "question": "HipSafetyService2 - Second Lorem ipsum this?",
-          "answer": "Lorem ipsum descrpition"
-        }
-      ]
-    }
-  ]
-};
+var basicComponents = [
+  {
+    title: "Introduction",
+    filePath: "dorel-component-guide-introduction.html"
+  },
+  {
+    title: "Typography",
+    filePath: "dorel-component-guide-typography.html"
+  },
+  {
+    title: "Layout & Sizes",
+    filePath: "dorel-component-guide-layout.html"
+  },
+  {
+    title: "Colors",
+    filePath: "dorel-component-guide-colors.html"
+  },
+  {
+    title: "Buttons",
+    filePath: "dorel-component-guide-buttons.html"
+  },
+  {
+    title: "Forms",
+    filePath: "dorel-component-guide-forms.html"
+  },
+  {
+    title: "Breakpoints",
+    filePath: "dorel-component-guide-breakpoints.html"
+  },
+  {
+    title: "Images",
+    filePath: "dorel-component-guide-images.html"
+  },
+  {
+    title: "Accordions",
+    filePath: "dorel-component-guide-accordions.html"
+  }
+];
