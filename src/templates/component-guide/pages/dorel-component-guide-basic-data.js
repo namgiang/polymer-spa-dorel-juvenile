3b65841ad9f2f121e3f84c0dee665f35cc571a08
Vincent Royen <vince@royen.nl>
2017-10-19 11:07:10 +0000

    polymer 2 changes (basic components)

---
+++ src/templates/component-guide/pages/dorel-component-guide-basic-data.js
@@ -0,0 +1,73 @@
+var basicComponents = {
+  "id": 1,
+  "brand_id": 4,
+  "name": "Services",
+  "value": "services",
+  "sub_categories": [
+    {
+      "id": 10,
+      "service_topics_category_id": 1,
+      "name": "Introduction",
+      "items": [
+        {
+          "id": 1,
+          "service_topics_sub_category_id": 10,
+          "featured": 1,
+          "question": "Repairs1 - First Lorem ipsum this?",
+          "answer": "Lorem ipsum descrpition"
+        },
+
+        {
+          "id": 2,
+          "service_topics_sub_category_id": 10,
+          "featured": 0,
+          "question": "Repairs2 - Fourth Lorem ipsum this?",
+          "answer": "Lorem ipsum descrpition"
+        }
+      ]
+    },
+
+    {
+      "id": 20,
+      "service_topics_category_id": 1,
+      "name": "Typography",
+      "items": [
+        {
+          "id": 3,
+          "service_topics_sub_category_id": 20,
+          "featured": true,
+          "question": "SpareParts1 - Second Lorem ipsum this?",
+          "answer": "Lorem ipsum descrpition"
+        },
+        {
+          "id": 4,
+          "service_topics_sub_category_id": 20,
+          "question": "SpareParts2 - Second Lorem ipsum this?",
+          "answer": "Lorem ipsum descrpition"
+        }
+      ]
+    },
+
+    {
+      "id": 30,
+      "service_topics_category_id": 1,
+      "name": "Layout & Sizes",
+      "items": [
+        {
+          "id": 5,
+          "service_topics_sub_category_id": 30,
+          "featured": true,
+          "question": "HipSafetyService1 - Second Lorem ipsum this?",
+          "answer": "Lorem ipsum descrpition"
+        },
+        {
+          "id": 6,
+          "service_topics_sub_category_id": 30,
+          "featured": true,
+          "question": "HipSafetyService2 - Second Lorem ipsum this?",
+          "answer": "Lorem ipsum descrpition"
+        }
+      ]
+    }
+  ]
+};
