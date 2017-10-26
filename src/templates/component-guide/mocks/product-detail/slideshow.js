3df17b3444f16e5b9ce4a08020b94510906674de
Aron Martin <mail@aronmartin.com>
2017-10-26 10:38:56 +0200

    DIO-1915 - Implement fullwidth slider

---
+++ src/templates/component-guide/mocks/product-detail/slideshow.js
@@ -0,0 +1,36 @@
+var slideshowMock = {
+  "slides": [
+      {
+        "type": 'image',
+        "bynder_id": '',
+        "image_url": 'http://www.planwallpaper.com/static/images/i-should-buy-a-boat.jpg',
+        "video_id": '',
+        "title": 'Full width image',
+        "description": 'This is a full width image with text'
+      },
+      {
+        "type": 'image',
+        "bynder_id": '',
+        "image_url": 'https://images5.alphacoders.com/436/thumb-1920-436482.jpg',
+        "video_id": '',
+        "title": '',
+        "description": ''
+      },
+      {
+        "type": 'video',
+        "bynder_id": '',
+        "image_url": 'https://static.pexels.com/photos/36764/marguerite-daisy-beautiful-beauty.jpg',
+        "video_id": '',
+        "title": 'Full width video with text',
+        "description": 'This is a full width video with text'
+      },
+      {
+        "type": 'video',
+        "bynder_id": '',
+        "image_url": 'http://www.planwallpaper.com/static/images/i-should-buy-a-boat.jpg',
+        "video_id": '',
+        "title": '',
+        "description": ''
+      }
+  ]
+};
