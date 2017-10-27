58b156c9998efc1a2c6db53d16321824255b296e
Aron Martin <mail@aronmartin.com>
2017-10-27 14:50:11 +0200

    DIO-1915 - WIP content-slider

--- src/templates/component-guide/mocks/product-detail/slideshow.js
+++ src/templates/component-guide/mocks/product-detail/slideshow.js
@@ -1,36 +1,29 @@
 var slideshowMock = {
   "slides": [
       {
-        "type": 'image',
-        "bynder_id": '',
-        "image_url": 'http://www.planwallpaper.com/static/images/i-should-buy-a-boat.jpg',
-        "video_id": '',
-        "title": 'Full width image',
-        "description": 'This is a full width image with text'
+        "type": "video",
+        "bynder_image_id": '7F84BD5F-551A-4DA5-8532AB44763439AA',
+        "video_id": 'A3PDXmYoF5U',
+        "content": {
+          "title": 'Full width video with text',
+          "description": 'This is a full width video with text',
+        }
       },
       {
-        "type": 'image',
-        "bynder_id": '',
+        "type": "image",
         "image_url": 'https://images5.alphacoders.com/436/thumb-1920-436482.jpg',
-        "video_id": '',
-        "title": '',
-        "description": ''
       },
       {
-        "type": 'video',
-        "bynder_id": '',
-        "image_url": 'https://static.pexels.com/photos/36764/marguerite-daisy-beautiful-beauty.jpg',
-        "video_id": '',
-        "title": 'Full width video with text',
-        "description": 'This is a full width video with text'
+        "type": "image",
+        "bynder_image_id": '7F84BD5F-551A-4DA5-8532AB44763439AA',
+        "content": {
+          "title": 'Full width image',
+          "description": 'This is a full width image with text'
+        }
       },
       {
-        "type": 'video',
-        "bynder_id": '',
-        "image_url": 'http://www.planwallpaper.com/static/images/i-should-buy-a-boat.jpg',
-        "video_id": '',
-        "title": '',
-        "description": ''
+        "type": "image",
+        "bynder_image_id": '7F84BD5F-551A-4DA5-8532AB44763439AA',
       }
   ]
 };
