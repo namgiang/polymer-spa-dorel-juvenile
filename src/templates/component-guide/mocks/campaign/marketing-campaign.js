84edc829a018d0daa484216404e377eb7022ef97
Michiel Reyers <m.reyers@tech.nl>
2017-10-30 09:05:17 +0100

    DIO-1464 marketing campaign work in progress

---
+++ src/templates/component-guide/mocks/campaign/marketing-campaign.js
@@ -0,0 +1,120 @@
+var marketingCampaignMock = {
+  "id": 26544,
+  "date": "2017-10-27T10:24:52",
+  "date_gmt": "2017-10-27T10:24:52",
+  "guid": {
+    "rendered": "http://dorel-spa.local:8080/?page_id=26544"
+  },
+  "modified": "2017-10-27T10:24:52",
+  "modified_gmt": "2017-10-27T10:24:52",
+  "slug": "campaign-page",
+  "status": "publish",
+  "type": "page",
+  "link": "http://dorel-spa.local:8080/uk-en/campaign-page/",
+  "title": {
+    "rendered": "Campaign page"
+  },
+  "content": {
+    "rendered": "",
+    "protected": false
+  },
+  "excerpt": {
+    "rendered": "",
+    "protected": false
+  },
+  "author": 1,
+  "featured_media": 0,
+  "parent": 0,
+  "menu_order": 0,
+  "comment_status": "closed",
+  "ping_status": "closed",
+  "template": "marketing-campaign",
+  "meta": [
+
+  ],
+  "acf": {
+    "page_topic": "NO VALUE SUITABLE",
+    "meta_title": "",
+    "meta_description": "",
+    "hero_header": [
+      {
+        "bynder_background_image": "14BB9491-A186-40D8-B965680777095B8E",
+        "title": "Meet Maxi Cosi strollers",
+        "sub_title" : "We carry the future",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        "video-id": "0zUUVyWAyRs"
+      }
+    ],
+    "marketing_header": {
+      "bynder_background_image": "14BB9491-A186-40D8-B965680777095B8E",
+      "title": "Explore the great outdoors",
+      "icon_list_items" : [
+        { "icon": "dorel-icons:play-circle-outline",
+      "text" : "Unique handfree holding" }
+      ],
+      cta_text : "Meet Outdoor Strollers",
+      cta_link : "/uk-en/car-seats/euro-nxt"
+    },
+
+    "marketing_builder": [
+      {
+        "acf_fc_layout": "content_blcok",
+        "title": "Lorem ipsum",
+        "description": "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris accumsan odio odio, ac finibus elit imperdiet id. Duis varius, ante laoreet hendrerit commodo, metus dolor consectetur enim, gravida sagittis mauris nisl eu lectus.</p>\n",
+        "image": "http://media.maxi-cosi.com/nl-nl/~/media/assets/media/main-collection-image/1303710110_2018_maxicosi_stroller_travelsystem_nova4_black_nomadblack_3qrt.ashx?vs=1&d=20170618T214140Z&mh=382&jpeg=85"
+      },
+      {
+        "acf_fc_layout": "content_blcok",
+        "title": "Lorem ipsum",
+        "description": "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris accumsan odio odio, ac finibus elit imperdiet id. Duis varius, ante laoreet hendrerit commodo, metus dolor consectetur enim, gravida sagittis mauris nisl eu lectus.</p>\n",
+        "image": "http://media.maxi-cosi.com/nl-nl/~/media/assets/media/main-collection-image/1303710110_2018_maxicosi_stroller_travelsystem_nova4_black_nomadblack_3qrt.ashx?vs=1&d=20170618T214140Z&mh=382&jpeg=85"
+      }
+    ]
+  },
+  "_links": {
+    "self": [
+      {
+        "href": "http://dorel-spa.local:8080/uk-en/wp-json/wp/v2/pages/26544"
+      }
+    ],
+    "collection": [
+      {
+        "href": "http://dorel-spa.local:8080/uk-en/wp-json/wp/v2/pages"
+      }
+    ],
+    "about": [
+      {
+        "href": "http://dorel-spa.local:8080/uk-en/wp-json/wp/v2/types/page"
+      }
+    ],
+    "author": [
+      {
+        "embeddable": true,
+        "href": "http://dorel-spa.local:8080/uk-en/wp-json/wp/v2/users/1"
+      }
+    ],
+    "replies": [
+      {
+        "embeddable": true,
+        "href": "http://dorel-spa.local:8080/uk-en/wp-json/wp/v2/comments?post=26544"
+      }
+    ],
+    "version-history": [
+      {
+        "href": "http://dorel-spa.local:8080/uk-en/wp-json/wp/v2/pages/26544/revisions"
+      }
+    ],
+    "wp:attachment": [
+      {
+        "href": "http://dorel-spa.local:8080/uk-en/wp-json/wp/v2/media?parent=26544"
+      }
+    ],
+    "curies": [
+      {
+        "name": "wp",
+        "href": "https://api.w.org/{rel}",
+        "templated": true
+      }
+    ]
+  }
+}
