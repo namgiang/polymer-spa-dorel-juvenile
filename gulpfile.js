c8b5cc186ad431559a277cb554f3e304dae3d57f
Aron Martin <mail@aronmartin.com>
2017-12-11 14:53:47 +0100

    DIO-2060 - gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -42,114 +42,6 @@ function waitFor(stream) {
 gulp.task('build:es5', function () {
   return new Promise((resolve, reject) => { // eslint-disable-line no-unused-vars
 
-<<<<<<< HEAD
-    const buildDirectory = 'build/es5';
-
-    // Lets create some inline code splitters in case you need them later in your build.
-    let sourcesStreamSplitter = new polymerBuild.HtmlSplitter();
-    let dependenciesStreamSplitter = new polymerBuild.HtmlSplitter();
-
-    // Okay, so first thing we do is clear the build directory
-    console.log(`Deleting ${buildDirectory} directory...`);
-    del([buildDirectory])
-      .then(() => {
-
-        // Let's start by getting your source files. These are all the files
-        // in your `src/` directory, or those that match your polymer.json
-        // "sources"  property if you provided one.
-        let sourcesStream = polymerProject.sources()
-
-          // If you want to optimize, minify, compile, or otherwise process
-          // any of your source code for production, you can do so here before
-          // merging your sources and dependencies together.
-          .pipe(gulpif(/\.(png|gif|jpg|svg)$/, imagemin()))
-
-          // The `sourcesStreamSplitter` created above can be added here to
-          // pull any inline styles and scripts out of their HTML files and
-          // into seperate CSS and JS files in the build stream. Just be sure
-          // to rejoin those files with the `.rejoin()` method when you're done.
-          .pipe(sourcesStreamSplitter.split())
-
-          .pipe(gulpif(/\.css$/, cleanCSS()))
-          .pipe(gulpif(/\.html$/, htmlmin({
-            collapseWhitespace: true,
-            removeComments: true,
-            minifyCSS: true,
-            uglifyJS: true
-          })))
-          .pipe(gulpif(/\.js$/, babel({
-            babelrc: false,
-            presets: [
-              ['env', {
-                'modules': false,
-                'targets': {
-                  'browsers': ['ie 11']
-                }
-              }],
-              'stage-3'
-            ]
-          })))
-          .pipe(gulpif(/\.js$/, uglify()))
-
-          // Uncomment these lines to add a few more example optimizations to your source files.
-          // .pipe(gulpif(/\.js$/, uglify())) // Install gulp-uglify to use
-          // .pipe(gulpif(/\.css$/, cssSlam())) // Install css-slam to use
-          // .pipe(gulpif(/\.html$/, htmlMinifier())) // Install gulp-html-minify to use
-
-          // Remember, you need to rejoin any split inline code when you're done.
-          .pipe(sourcesStreamSplitter.rejoin());
-
-
-        // Similarly, you can get your dependencies seperately and perform
-        // any dependency-only optimizations here as well.
-        let dependenciesStream = polymerProject.dependencies()
-          .pipe(dependenciesStreamSplitter.split())
-
-          // Add any dependency optimizations here.
-          .pipe(gulpif(/\.css$/, cleanCSS()))
-          .pipe(gulpif(/\.html$/, htmlmin({
-            collapseWhitespace: true,
-            removeComments: true,
-            minifyCSS: true,
-            uglifyJS: true
-          })))
-          .pipe(gulpif(/\.js$/, babel({
-            babelrc: false,
-            presets: [ [ 'env', { modules: false } ],  'stage-3' ]
-          })))
-          .pipe(gulpif(/\.js$/, uglify()))
-
-          .pipe(dependenciesStreamSplitter.rejoin());
-
-
-        // Okay, now let's merge your sources & dependencies together into a single build stream.
-        let buildStream = mergeStream(sourcesStream, dependenciesStream)
-          .once('data', () => {
-            console.log('Analyzing build dependencies...');
-          });
-
-        // If you want bundling, pass the stream to polymerProject.bundler.
-        // This will bundle dependencies into your fragments so you can lazy
-        // load them.
-        buildStream = buildStream.pipe(polymerProject.bundler());
-        
-        buildStream = buildStream.pipe(polymerProject.addCustomElementsEs5Adapter())
-
-        // Okay, time to pipe to the build directory
-        buildStream = buildStream.pipe(gulp.dest(buildDirectory));
-
-        // waitFor the buildStream to complete
-        return waitFor(buildStream);
-      })
-      .then(() => {
-        // Okay, now let's generate the Service Worker
-        console.log('Generating the Service Worker...');
-        return polymerBuild.addServiceWorker({
-          project: polymerProject,
-          buildRoot: buildDirectory,
-          bundled: true,
-          swPrecacheConfig: swPrecacheConfig
-=======
   mkdirp(__dirname + '/build/log/', (err) => {
     if (err) console.error(err)
     else console.log('Log directory created!')
@@ -166,7 +58,7 @@ gulp.task('build:es5', function () {
       log_file.write(util.format(d) + '\n');
       log_stdout.write(util.format(d) + '\n');
     };
-    
+
     console.warn = (d) => { //
       log_file.write(util.format(d) + '\n');
       log_stdout.write(util.format(d) + '\n');
@@ -256,14 +148,13 @@ gulp.task('build:es5', function () {
       let buildStream = mergeStream(sourcesStream, dependenciesStream)
         .once('data', () => {
           console.log('Analyzing build dependencies...');
->>>>>>> develop
         });
 
       // If you want bundling, pass the stream to polymerProject.bundler.
       // This will bundle dependencies into your fragments so you can lazy
       // load them.
       buildStream = buildStream.pipe(polymerProject.bundler());
-      
+
       buildStream = buildStream.pipe(polymerProject.addCustomElementsEs5Adapter())
 
       // Okay, time to pipe to the build directory
@@ -294,7 +185,7 @@ gulp.task('build:es5', function () {
 });
 
 gulp.task('build:es6', function(done) {
-  
+
   mkdirp(__dirname + '/build/log/', (err) => {
     if (err) console.error(err)
     else console.log('Log directory created!')
